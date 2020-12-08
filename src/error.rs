bd62148a5301e1fb5edf2e836ac2aa7cb4e1e1ea
Art Deineka <darkruby@mbp.local>
2020-12-08 20:41:06 +0000

    attempt migrate Y!1.1.0

--- src/error.rs
+++ src/error.rs
@@ -1,7 +1,6 @@
 use actix_web::ResponseError;
 use std::error::Error;
 use std::fmt::{Display, Formatter, Result};
-use yahoo_finance_api as yahoo;
 
 #[derive(Debug)]
 pub struct ApiError {
@@ -24,45 +23,21 @@ impl Display for ApiError {
 
 impl Error for ApiError {}
 
-impl From<&str> for ApiError {
-  fn from(s: &str) -> ApiError {
+impl<T> From<T> for ApiError
+where
+  T: Into<String>,
+{
+  fn from(err: T) -> Self {
     ApiError {
-      message: String::from(s),
+      message: err.into(),
     }
   }
 }
 
-impl From<String> for ApiError {
-  fn from(message: String) -> ApiError {
-    ApiError { message }
-  }
-}
-
-impl From<yahoo::YahooError> for ApiError {
-  fn from(ye: yahoo::YahooError) -> ApiError {
-    match ye {
-      yahoo::YahooError::FetchFailed(code) => ApiError::from(format!("FetchFailed {}", code)),
-      yahoo::YahooError::DeserializeFailed(mess) => {
-        ApiError::from(format!("DeserializeFailed {}", mess))
-      }
-      yahoo::YahooError::ConnectionFailed => ApiError::from("ConnectionFailed"),
-      yahoo::YahooError::InvalidStatusCode => ApiError::from("InvalidStatusCode"),
-      yahoo::YahooError::EmptyDataSet => ApiError::from("EmptyDataSet"),
-      yahoo::YahooError::DataInconsistency => ApiError::from("DataInconsistency"),
-    }
-  }
-}
-
-impl From<regex::Error> for ApiError {
-  fn from(_re: regex::Error) -> ApiError {
-    ApiError::from("regex::Error")
-  }
-}
-
-impl From<std::num::ParseIntError> for ApiError {
-  fn from(_pe: std::num::ParseIntError) -> ApiError {
-    ApiError::from("ParseIntError")
-  }
-}
+// impl From<yahoo::YahooError> for ApiError {
+//   fn from(ye: yahoo::YahooError) -> ApiError {
+//     ApiError::from(format!("{}", ye))
+//   }
+// }
 
 impl ResponseError for ApiError {}
