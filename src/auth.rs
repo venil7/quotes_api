146860f0230a2ac049c1af8b20f9cdfc249db6c6
Art Deineka <darkruby@mbp.local>
2020-12-13 11:30:22 +0000

    auth mddleware return 401 on failure

--- src/auth.rs
+++ src/auth.rs
@@ -1,11 +1,11 @@
 use crate::auth_api::AuthApi;
-use crate::error::ApiError;
-use actix_web::dev::ServiceRequest;
+use actix_web::dev::{HttpResponseBuilder, ServiceRequest};
+use actix_web::http::StatusCode;
 use actix_web::Error;
 use actix_web_httpauth::extractors::basic::BasicAuth;
 use actix_web_httpauth::extractors::bearer::BearerAuth;
 
-pub async fn _basic_validator(
+pub async fn basic_validator(
   req: ServiceRequest,
   _credentials: BasicAuth,
 ) -> Result<ServiceRequest, Error> {
@@ -17,11 +17,12 @@ pub async fn bearer_validator(
   credentials: BearerAuth,
 ) -> Result<ServiceRequest, Error> {
   if let Some(auth_api) = req.app_data::<AuthApi>() {
-    match auth_api.validate_token(credentials.token()) {
-      Ok(_claims) => Ok(req),
-      Err(err) => Err(Error::from(ApiError::from(format!("{}", err)))),
+    if let Ok(_claims) = auth_api.validate_token(credentials.token()) {
+      // _claims are available here,
+      // and can be attached to req, to be passed further down
+      return Ok(req);
     }
-  } else {
-    Err(Error::from(ApiError::from("Auth api not available")))
   }
+  let response = HttpResponseBuilder::new(StatusCode::UNAUTHORIZED);
+  Err(Error::from(response))
 }
